f118baba5c1d37c2204285057dc7f0a5720c44db
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-12-15 08:53:09 +0200

    parent form

--- src/app/pages/service/flyout-service/flyout-service.component.ts
+++ src/app/pages/service/flyout-service/flyout-service.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, ComponentFactoryResolver, Injector, OnInit, TemplateRef, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { StepperService } from 'src/app/components/stepper/stepper.module';
+import { ServiceForm } from '../forms/service-form/service-form.component';
+import { StepperBase } from '../forms/stepper-base.interface';
+
+export class StepItem {
+  constructor(public component: Type<any>, public data: any){}
+}
 
 @Component({
   selector: 'flyout-service',
@@ -11,58 +17,50 @@ export class FlyoutServiceComponent implements OnInit {
 
   step: number;
 
-  public steps = [
-    {
-      title: 'Service',
-      active: false
-    },
-    {
-      title: 'Images',
-      active: false
-    },
-    {
-      title: 'Confirmation',
-      active: false
-    }
+  public steps: StepItem[] = [
+    new StepItem(ServiceForm, {title: 'Service'}),
+    new StepItem(ServiceForm, {title: 'Upload'}),
+    new StepItem(ServiceForm, {title: 'Confirmation'})
   ]
 
   serviceForm: FormGroup;
 
-  @ViewChild('serviceTemplate', { static: true })
-  serviceTemplate: TemplateRef<any>;
-
-  @ViewChild('productTemplate', { static: true })
-  productTemplate: TemplateRef<any>;
-
-  @ViewChild('confirmationTemplate', { static: true })
-  confirmationTemplate: TemplateRef<any>;
-
   @ViewChild('stepperContainer', { read: ViewContainerRef, static: true })
   stepperContainer: ViewContainerRef
 
-  constructor(private _stepperService: StepperService) { }
+  constructor(
+    private _stepperService: StepperService,
+    private _fb: FormBuilder,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) { }
 
   ngOnInit(): void {
     this._stepperService.navigation.subscribe(step => {
       this.step = step;
     });
 
+    
+
     this._stepperService.navigation.subscribe(step => {
       this.stepperContainer.clear();
-      switch(step){
+      switch (step) {
         case 0:
-          this.stepperContainer.createEmbeddedView(this.serviceTemplate);
+          // this.stepperContainer.createEmbeddedView(this.serviceTemplate);
+          const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.steps[0].component);
+          this.stepperContainer.clear();
+          const componentRef = this.stepperContainer.createComponent<StepperBase>(componentFactory);
+          debugger;
+          this.serviceForm = this._fb.group({
+            details: componentRef.instance.createFormgroup()
+          });
           break;
         case 1:
-          this.stepperContainer.createEmbeddedView(this.productTemplate);
+          // this.stepperContainer.createEmbeddedView(this.productTemplate);
           // this._stepperService.nextPage();
           break;
         case 2:
-          this.stepperContainer.createEmbeddedView(this.confirmationTemplate);
+          // this.stepperContainer.createEmbeddedView(this.confirmationTemplate);
       }
     })
   }
-
-
-
 }
--- src/app/pages/service/forms/service-form/service-form.component.ts
+++ src/app/pages/service/forms/service-form/service-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, Injector, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { StepperService } from 'src/app/components/stepper/stepper.module';
+import { StepperService } from './../../../../components/stepper/stepper.module';
 import { moneyFormatValidator } from './../../../../services/custom-validators/custom-validators.service';
 import { FormGroupConfig } from '../../../../model/form-group.config';
+import { StepperBase } from '../stepper-base.interface';
 
 export interface Service {
     name: string;
@@ -14,19 +15,28 @@ export interface Service {
     templateUrl: './service-form.component.html',
     styleUrls: ['./service-form.component.scss']
 })
-export class ServiceForm implements OnInit {
+export class ServiceForm implements StepperBase, OnInit {
+    private _fb: FormBuilder;
+    private _stepperService: StepperService
+
     serviceForm: FormGroup;
 
-    constructor(private _fb: FormBuilder, private _stepperService: StepperService){}
+    constructor(private injector: Injector){
+        this._fb = this.injector.get(FormBuilder);
+        this._stepperService = this.injector.get(StepperService);
+    }
 
-    ngOnInit(): void {this.createForm()}
+    steps: any[];
+    activeStep: number;
+
+    ngOnInit(): void {}
 
     get f() { return this.serviceForm.controls; }
 
-    createForm(){
+    createFormgroup() {
         const config: FormGroupConfig<Service> = {
             name: ['', [Validators.required]],
-            price: [null, [Validators.required, moneyFormatValidator]]
+            price: [null, [Validators.required]]
         }
         
         this.serviceForm = this._fb.group(config);
